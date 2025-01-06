ec230ebe65f884d1ae9d662f54cf9a92c6f1ddd8
jdav892 <jadavila9@gmail.com>
2025-01-05 20:07:52 -0500

    first

---
+++ scrapers/bstest.py
@@ -0,0 +1,20 @@
+#from bs4 import BeautifulSoup
+#import requests
+from datetime import datetime as dt
+#
+# 
+#URL = "https://www.basketball-reference.com/leagues/NBA_2025_per_game.html#per_game_stats::pts_per_g"
+# 
+#response = requests.get(URL)
+#url_page = response.text
+#
+#soup = BeautifulSoup(url_page, "html.parser")
+#
+#
+#ppg_list_left = soup.find_all(attrs="tbody", class_="first_place")
+#
+#ppg_list_right = soup.find_all(attrs="tbody", class_="first_place")
+#
+#print(ppg_list_left)
+#print(ppg_list_right)
+print(dt.today())
---
+++ scrapers/main.py
@@ -0,0 +1,58 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.basketball-reference.com/leagues/NBA_2025_leaders.html")
+""" TODO: Rewrite this Selenium crawler to click on each player's profile,
+and select out the img element for each league leader so I will have an img
+to display with the static data that beautiful soup scrapes.
+"""
+#point_search = driver.find_element(By.CSS_SELECTOR, value="div table tbody .text-right")
+#print(point_search.text)
+
+
+#TODO: store print values in variables to write to text file to plot data points later
+
+print("Scraping in Progress")
+def scoring_leader():
+    highest_ppg_name = driver.find_element(By.CSS_SELECTOR, value="#leaders_pts_per_g .first_place .who").text
+    highest_ppg_value = driver.find_element(By.CSS_SELECTOR, value="#leaders_pts_per_g .first_place .value").text
+    with open("ppg_leader.text", "w") as f:
+        f.write(f"{highest_ppg_name}, {highest_ppg_value}")
+    
+def rebounding_leader():
+    highest_trbpg_name = driver.find_element(By.CSS_SELECTOR, value="#leaders_trb_per_g .first_place .who").text
+    highest_trbpg_value = driver.find_element(By.CSS_SELECTOR, value="#leaders_trb_per_g .first_place .value").text
+    print(highest_trbpg_name, highest_trbpg_value)
+    
+def assist_leader():
+    highest_apg_name = driver.find_element(By.CSS_SELECTOR, value="#leaders_ast_per_g .first_place .who").text
+    highest_apg_value = driver.find_element(By.CSS_SELECTOR, value="#leaders_ast_per_g .first_place .value").text
+    print(highest_apg_name, highest_apg_value)
+    
+def steal_leader():
+    highest_stl_name = driver.find_element(By.CSS_SELECTOR, value="#leaders_stl_per_g .first_place .who").text
+    highest_stl_value = driver.find_element(By.CSS_SELECTOR, value="#leaders_stl_per_g .first_place .value").text
+    print(highest_stl_name, highest_stl_value)
+    
+def block_leader():
+    highest_blk_name = driver.find_element(By.CSS_SELECTOR, value="#leaders_blk_per_g .first_place .who").text
+    highest_blk_value = driver.find_element(By.CSS_SELECTOR, value="#leaders_blk_per_g .first_place .value").text
+    print(highest_blk_name, highest_blk_value)
+    
+scoring_leader()
+#rebounding_leader()
+#assist_leader()
+#steal_leader()
+#block_leader()
+
+driver.quit()
+
+if __name__ == "__main__":
+    print("Scrape Complete")
+
