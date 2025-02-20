ac0ecd8811283e9bb4ef15a70c5272789d7bb5c2
jdav892 <jadavila9@gmail.com>
2025-02-19 21:52:49 -0500

    feat: removed keys for selenium driver where not needed

--- scrapers/ppg.py
+++ scrapers/ppg.py
@@ -1,6 +1,5 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from decorators import writer
 
 chrome_options = webdriver.ChromeOptions()
